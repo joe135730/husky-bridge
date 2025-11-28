4514a08e5d65060aa7d190678299dbadaa38d1c7
Cheyi Wu <wu.chey@northeastern.edu>
2025-11-28 17:10:58 -0500

    setup test environment

---
+++ src/test/setupTests.ts
@@ -0,0 +1,8 @@
+// test initialization, test environment setup.
+// It runs before each test file.
+// Here we import @testing-library/jest-dom, which:
+// Adds extra matchers to expect, like:
+// toBeInTheDocument()
+// toHaveTextContent()
+// toBeVisible()
+import '@testing-library/jest-dom'
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// Test
+import { configDefaults } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  /*
+    test: { ... }
+      This block is Vitest configuration inside your Vite config.
+    environment: 'jsdom'
+      Vitest will run tests in a fake browser environment provided by jsdom.
+      Required if you test React components that use document, window, etc.
+    globals: true
+      Lets you write tests without importing describe, it, expect in every file.
+      (We imported them manually in the example, but with globals: true you can skip the import.)
+    setupFiles: './src/test/setupTests.ts'
+      Tells Vitest:
+        “Before running tests, execute setupTests.ts.”
+    exclude: [...configDefaults.exclude, 'e2e/**']
+      Keeps default excludes (like node_modules) and also ignores e2e directory if you later add Playwright/Cypress tests.
+  */
+    test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setupTests.ts',
+    exclude: [...configDefaults.exclude, 'e2e/**'],
+  },
   server: {
     proxy: {
       '/api': {
